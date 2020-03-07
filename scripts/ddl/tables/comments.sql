965cdbe2a792fc0cedbb5aef5fcd416ebf8a7604
Hansel <hansel.donizete@gmail.com>
2020-03-07 17:51:51 -0300

    initial commit

---
+++ scripts/ddl/tables/comments.sql
@@ -0,0 +1,6 @@
+--COMENTÁRIOS DAS TABELAS/COLUNA
+COMMENT ON TABLE GAMES.JOGOS IS
+'Descrição da tabela jogos...';
+
+COMMENT ON COLUMN GAMES.JOGOS.JGS_ID IS
+'Descrição do atributo ID da tabela jogos...';
